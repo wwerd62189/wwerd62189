2db674b811a1c6865117dd2001a676f0fb890dfb
wwerd62189 <37175991+wwerd62189@users.noreply.github.com>
2018-03-15 17:57:26 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double BMI ;
+            Console.Write("身高:" );
+            string a = Console.ReadLine();
+
+            Console.Write("體重:");
+            string b = Console.ReadLine();
+
+            double c = b / (a / 100) ^ 2;
+
+            string d = Console.ReadLine("BMI:"+c);
+
+            
+
+
+        }
+    }
+}
